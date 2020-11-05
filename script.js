60c1ea1b64309e97b3e03157c8541ba60b70464e
NaiChang <nainai0123@gmail.com>
2020-11-05 21:38:32 +0800

    fini

---
+++ script.js
@@ -0,0 +1,58 @@
+// 語法
+// if (布林值) {程式區塊}
+// 布林值為 true 才會執行 程式區塊
+if (true) {
+  console.log("我是判斷式");
+}
+
+var price1 = 500;
+
+if (price1 >= 200) {
+  // 布林值 false 才會執行 else { 程式區塊 }
+  var price1 = 50;
+
+  if (price1 >= 200) {
+    console.log("大於 200 元的商品");
+  }
+  else {
+    console.log("小於 200 元的商品");
+  }
+
+  // else if 次數無限，但條件需不同
+  // 語法: even if (布林值) {}
+  // else if 布林值為 true 才會執行  else if (布林值) { 程式區塊 }
+  var score = 100;
+
+  if (score >= 60) {
+    console.log("及格");
+  }
+  else if (score >= 40) {
+    console.log("補考");
+  }
+
+  else {
+    console.log("當掉");
+  }
+
+
+  // 陣列:多個相同類型的資料
+  var boxes = document.getElementsByClassName("box");
+
+  console.log(boxes[1]);
+
+  boxes[0].innerHTML = "我是盒子";
+  boxes[1].innerHTML = "我是盒子";
+  boxes[2].innerHTML = "我是盒子";
+
+  // for 迴圈
+  // 語法: for (初始值；條件；每次執行)
+  for (var i = 0; i < 10; i++) {
+    console.log("迴圈執行次數:" + i);
+  }
+
+  // 陣列 長度 length :陣列的數量
+  console.log(" 陣列的數量: " boxes.length);
+
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = " 用迴圈修改內容的盒子 " + (i + 1);
+  }
